bd5112862fd91a25293e7cb2a3253ffcd4654048
ssini <cdnnnl@ewhain.net>
2021-11-15 00:00:31 +0900

    ✨ feat: Add description in footer

--- moonda-funding/src/components/Footer.js
+++ moonda-funding/src/components/Footer.js
@@ -13,24 +13,36 @@ const Wrapper = styled.div`
 `;
 
 const Contact = styled.div`
-  bottom: 0;
   align-self: flex-start;
   color: grey;
+  font-size: 8px;
 `;
 
 const Button = styled.button`
   border: 1px solid black;
-  border-radius: 10px;
+  border-radius: 50px;
+  width: 280px;
   background-color: white;
   color: black;
+  font-weight: bold;
   font-size: 2rem;
-  font-size: 20px;
+  padding: 18px;
+  margin: 40px 20px;
+  font-size: 15px;
+`;
+
+const Message = styled.div`
+  font-size: 10px;
+  color: grey;
 `;
 
 const Footer = () => {
   return (
     <Wrapper>
-      <Button>앱 출시 알림 신청받기</Button>
+      <Button>
+        <Message>상위 1% 튜터의 음악 & 미술 클래스! </Message>앱 출시 알림
+        신청받기
+      </Button>
       <Contact>Contact</Contact>
       <Contact>jhshin.moonda@gmail.com</Contact>
       <Contact>jsyang.moonda@gmail.com</Contact>
