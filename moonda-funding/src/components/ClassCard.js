444cc8f4bfee25106e050ea685c4dc311603bf41
ssini <cdnnnl@ewhain.net>
2021-11-14 23:54:42 +0900

    ✨ feat: Add ClassCard component

--- moonda-funding/src/components/ClassCard.js
+++ moonda-funding/src/components/ClassCard.js
@@ -1,74 +1,86 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import ProgressBar from "./ProgressBar";
 import { useNavigate } from "react-router-dom";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px solid black;
-  width: 100px;
-  font-size: 10px;
-  height: 200px;
-  padding: 1rem;
-  border-radius: 3px;
-  font-size: 2rem;
-  padding: 2rem;
-`;
-
-const Thumbnail = styled.div`
-  height: 40%;
-  border: 1px solid black;
-  padding: 1rem;
-  border-radius: 3px;
-
-  padding: 2rem;
-`;
-const Desc = styled.div`
-  height: 10%;
-  border: 1px solid black;
-  font-size: 10px;
-  border-radius: 3px;
-`;
-const AchieveRate = styled.div`
-  border: 1px solid black;
-  font-size: 10px;
-  border-radius: 3px;
-`;
-const ProgressBar = styled.div`
-  border: 1px solid black;
-  border-radius: 3px;
-  background-color: red;
-`;
-const CheerButton = styled.button`
-  border: 1px solid black;
-  border-radius: 3px;
-  font-size: 2rem;
-  font-size: 10px;
-`;
-
 function StyledCard({ name, desc, cheerCount, goalCount }) {
-  const [classInfo, setClassInfo] = useState([{ name: name }, { desc: desc }]);
+  const [classInfo, setClassInfo] = useState([name, desc]);
   const [count, setCount] = useState(cheerCount);
   const navigate = useNavigate();
 
   const handleCheerCount = () => {
     setCount(cheerCount + 1);
   };
-
   return (
     <Wrapper>
       <Thumbnail />
+      <Name> {name}</Name>
+
       <Desc>{desc}</Desc>
-      <AchieveRate>달성율 {(cheerCount / goalCount) * 100}%</AchieveRate>
-      <ProgressBar />
-      <CheerButton onClick={handleCheerCount}>{count}명 응원중</CheerButton>
-      <CheerButton
-        onClick={() => navigate("/detail", { state: { classInfo: classInfo } })}
-      >
-        응원하기
+      <ProgressBar cheerCount={cheerCount} goalCount={goalCount} />
+      <CheerButton onClick={handleCheerCount}>
+        <Red>{count}명</Red> 응원중👏
       </CheerButton>
+      <GotoCheerButton
+        onClick={() => navigate("/detail", { state: { classInfo } })}
+      >
+        응원하기 {">"}
+      </GotoCheerButton>
     </Wrapper>
   );
 }
 
 export default StyledCard;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: inherit;
+  gap: 0.2rem;
+  height: 250px;
+  border-radius: 3px;
+  font-size: 2rem;
+  padding: 5px;
+  margin: 10px;
+`;
+
+const Thumbnail = styled.div`
+  height: 70%;
+  width: inherit;
+  background-color: lightgrey;
+`;
+const Name = styled.div`
+  height: 10%;
+  font-weight: bold;
+
+  font-size: 15px;
+`;
+
+const Desc = styled.div`
+  height: 10%;
+  font-size: 10px;
+  font-weight: bold;
+`;
+
+const Red = styled.span`
+  color: red;
+`;
+
+const CheerButton = styled.button`
+  border-radius: 10px;
+  height: 5rem;
+  font-weight: bold;
+  background-color: white;
+  font-size: 13px;
+  border: none;
+  box-shadow: 5px 5px 20px 0 lightgrey;
+`;
+
+const GotoCheerButton = styled.button`
+  height: 1rem;
+  font-weight: bold;
+  border: none;
+  margin-top: 5px;
+  background-color: white;
+  font-size: 10px;
+`;
