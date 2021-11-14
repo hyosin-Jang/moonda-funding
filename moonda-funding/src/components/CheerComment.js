241befe7a40847803b46fa2dba5424bede4c78db
ssini <cdnnnl@ewhain.net>
2021-11-14 23:54:17 +0900

    ✨ feat: Add CheerComment component

--- moonda-funding/src/components/CheerComment.js
+++ moonda-funding/src/components/CheerComment.js
@@ -3,11 +3,17 @@ import styled from "styled-components";
 import CommentBoard from "./CommentBoard";
 const Wrapper = styled.div`
   display: flex;
-
+  padding: 13px;
+  margin: 7px;
   flex-direction: column;
 
   width: 100%;
-  height: 80%;
+  height: 60%;
+`;
+
+const Red = styled.div`
+  color: red;
+  font-weight: bold;
 `;
 
 // 컨테이너에서 API 호출하기
@@ -16,7 +22,7 @@ const Wrapper = styled.div`
 const CheerComment = () => {
   return (
     <Wrapper>
-      기대 한마디
+      <Red>기대 한마디</Red>
       <br />
       <CommentBoard />
     </Wrapper>
