fc5c9f514359b940722e3a0c9bfbfed48eac6344
ssini <cdnnnl@ewhain.net>
2021-11-20 04:28:50 +0900

    ✨ feat: Add show more button

--- moonda-funding/src/components/CommentBoard.js
+++ moonda-funding/src/components/CommentBoard.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  width: 100%;
   flex-direction: column;
-  width: 80%;
   margin: 10px;
 `;
 
@@ -12,34 +13,35 @@ const Name = styled.span`
   font-weight: bold;
 `;
 
+const MoreButton = styled.div`
+  display: flex;
+  border: 0;
+  align-self: center;
+  background-color: none;
+  font-size: 15px;
+  cursor: pointer;
+`;
+
 const Comment = styled.span`
   color: grey;
 `;
 
-const ex = [
-  {
-    id: 0,
-    nickname: "솜사탕이 먹고 싶은 모네",
-    comment: "헐 제 인생 뮤지컬 넘버를 이렇게 배울 수 있다니...!!"
-  },
-  {
-    id: 1,
-    nickname: "핸드폰 충전 중인 반 고흐",
-    comment:
-      "항상 뮤지컬에 대해 어렵다 생각하고 도전도 안해봤는데.. 좋은 기회가 될 것 같아요"
-  }
-];
-
 const CommentBoard = ({ comments }) => {
+  const [visible, setVisible] = useState(6);
+  const showMoreItems = () => {
+    setVisible(preValue => preValue + 6);
+  };
+
   return (
     <Wrapper>
-      {comments.map((comments, index) => (
+      {comments.slice(0, visible).map((comments, index) => (
         <div key={index}>
-          <Name>{comments.nickname}</Name> :
+          <Name>{comments.nickname}</Name> :{" "}
           <Comment>{comments.comment}</Comment>
           <br /> <br />
         </div>
       ))}
+      <MoreButton onClick={showMoreItems}>더보기</MoreButton>
     </Wrapper>
   );
 };
