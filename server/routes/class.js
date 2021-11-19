ccc915f7102f3a8eaf8b875d630bd1c523f78d2c
ssini <cdnnnl@ewhain.net>
2021-11-19 23:48:44 +0900

    🚧 feat: Add class api routes

--- server/routes/class.js
+++ server/routes/class.js
@@ -1,96 +1,43 @@
 const express = require('express');
+const {Class, Comment }= require('../models');
 
 const router = express.Router();
 
 // GET 라우터
-router.get('/', (req,res)=>{
+
+router.route('/')
+.get(async (req, res, next) => {
+  try{
+    const classes = await Class.findAll();
     res.json(classes);
+  } catch(err){
+    console.error(err);
+    next(err);
+  }
+})
+
+// POST - 클래스 등록 api
+.post(async(req, res, next)=> {
+  try{
+    const newClass = await Class.create({
+      className: req.body.className,
+      subtitle: req.body.subtitle,
+      goalCount: req.body.goalCount,
+      cheerCount: req.body.cheerCount,
+      desc: req.body.desc,
+      isAchieve: req.body.isAchieve,
+      manager: req.body.manager,
+      isActive: req.body.isActive
+    });
+    console.log(newClass);
+    res.status(201).json(newClass);
+  } catch(err){
+    console.error(err);
+    next(err);
+  }
 });
 
-const classes = [
-    {
-      no: 1,
-      className: "최애 뮤지컬 넘버 배우기",
-      desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-      no: 2,
-  
-      className: "JAZZ 속으로",
-      desc: "마음 속 품어둔 재즈 음악 한곡 완주하기",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-      no: 3,
-      className: "최애 뮤지컬 넘버 배우기",
-      desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-      no: 4,
-      className: "최애 뮤지컬 넘버 배우기",
-      desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-      no: 5,
-      className: "JAZZ속으로",
-      desc: "마음 속 품어둔 재즈 음악 한곡 완주하기",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-      no: 6,
-      className: "최애 뮤지컬 넘버 배우기",
-      desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-      no: 7,
-      className: "최애 뮤지컬 넘버 배우기",
-      desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-      cheerCount: 27,
-      goalCount: 100,
-      introduction:
-        "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-    },
-    {
-        no: 8,
-        className: "최애 뮤지컬 넘버 배우기",
-        desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-        cheerCount: 27,
-        goalCount: 100,
-        introduction:
-          "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-      },
-      {
-        no: 9,
-        className: "최애 뮤지컬 넘버 배우기",
-        desc: "마음 속 품어둔 뮤지컬 넘버 한곡 완주",
-        cheerCount: 27,
-        goalCount: 100,
-        introduction:
-          "항상 마음 속에만 품어두었던 노래를 직접 뮤지컬 배우가 되어 불러보아요!"
-      }
-  ];
+
   
 
 module.exports = router;
