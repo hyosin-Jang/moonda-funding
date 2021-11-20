2a8a2ae72ea20a1b05e8c5dbfb99d8dca9858786
ssini <cdnnnl@ewhain.net>
2021-11-20 20:37:07 +0900

    💄 style: Update footer UI

--- moonda-funding/src/components/Footer.js
+++ moonda-funding/src/components/Footer.js
@@ -4,18 +4,25 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   bottom: 0;
   display: flex;
-  height: 200px;
+  height: 280px;
   flex-direction: column;
   align-items: center;
   width: 100%;
   background-color: black;
   justify-content: center;
+  margin-top: 30px;
 `;
 
 const Contact = styled.div`
   align-self: flex-start;
   color: grey;
-  font-size: 8px;
+  width: 90%;
+  font-size: 13px;
+  line-height: 20px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  border-top: 1px solid grey;
+  margin: 20px;
 `;
 
 const Button = styled.button`
@@ -25,14 +32,14 @@ const Button = styled.button`
   background-color: white;
   color: black;
   font-weight: bold;
-  font-size: 2rem;
   padding: 18px;
-  margin: 40px 20px;
-  font-size: 15px;
+  margin-top: 40px;
+  margin-bottom: 10px;
+  font-size: 18px;
 `;
 
 const Message = styled.div`
-  font-size: 10px;
+  font-size: 12px;
   color: grey;
 `;
 
@@ -43,9 +50,11 @@ const Footer = () => {
         <Message>상위 1% 튜터의 음악 & 미술 클래스! </Message>앱 출시 알림
         신청받기
       </Button>
-      <Contact>Contact</Contact>
-      <Contact>jhshin.moonda@gmail.com</Contact>
-      <Contact>jsyang.moonda@gmail.com</Contact>
+      <Contact>
+        Contact <br />
+        jhshin.moonda@gmail.com <br />
+        jsyang.moonda@gmail.com
+      </Contact>
     </Wrapper>
   );
 };
