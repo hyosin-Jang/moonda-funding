e00464b4436f45a5d654e527baa7aaff4e3860b8
ssini <cdnnnl@ewhain.net>
2021-11-22 22:11:25 +0900

    🐛 Fix header css

--- moonda-funding/src/components/DetailHeader.js
+++ moonda-funding/src/components/DetailHeader.js
@@ -4,23 +4,6 @@ import "../style/style.scss";
 import backicon from "../image/back-icon.jpg";
 import { useNavigate } from "react-router-dom";
 
-const Wrapper = styled.div`
-  top: 0;
-  display: flex;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  background-color: white;
-  padding: 8px;
-  height: 50px;
-  align-items: center;
-  border-bottom-style: solid;
-  border-bottom-color: lightgrey;
-  border-bottom-width: 1px;
-  font-weight: bold;
-  justify-content: space-between;
-`;
-
 const BackIcon = styled.img`
   width: 30px;
   height: 30px;
@@ -36,11 +19,11 @@ const DetailHeader = ({ name }) => {
   const navigate = useNavigate();
 
   return (
-    <Wrapper>
+    <div className="detail-header">
       <BackIcon src={backicon} onClick={() => navigate("/")} />
       <Name>{name}</Name>
-      <div></div>
-    </Wrapper>
+      <div> </div>
+    </div>
   );
 };
 
